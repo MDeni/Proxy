5ded3da762da62bf6783de62d2d9bd71f70c81b5
Denis M <denis.muehlon@web.de>
2018-03-06 10:36:20 -0800

    origin port secure false

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
-port=443;
+//port=443;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
@@ -24,17 +24,17 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  //requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
-//  removeHeaders: [
-//    'cookie',
-//    'cookie2',
-//    // Strip Heroku-specific headers
-//    'x-heroku-queue-wait-time',
-//    'x-heroku-queue-depth',
-//    'x-heroku-dynos-in-use',
-//    'x-request-start',
-//  ],
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-heroku-queue-wait-time',
+    'x-heroku-queue-depth',
+    'x-heroku-dynos-in-use',
+    'x-request-start',
+  ],
   redirectSameOrigin: true,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
