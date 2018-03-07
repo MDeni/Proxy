c7447995843bd46b9a09ff352d0bd164fb2d1257
Denis M <denis.muehlon@web.de>
2018-03-07 14:18:24 -0800

    requestedorigin

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   //requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: ['x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
